8d714dec4f392a04bc71ef8a954f22219d0e9162
MASTER <MASTER@DESKTOP-PLC0S76>
2023-06-13 22:54:29 +0900

    초기 커밋.

---
+++ numberpuzzle/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace numberpuzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btn_mx = new System.Windows.Forms.Button();
+            this.btn_ex = new System.Windows.Forms.Button();
+            this.lb_ct = new System.Windows.Forms.Label();
+            this.lb_rule = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightGray;
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Location = new System.Drawing.Point(24, 19);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(526, 463);
+            this.panel1.TabIndex = 0;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("맑은 고딕", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button5.Location = new System.Drawing.Point(183, 163);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(160, 140);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("맑은 고딕", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button4.Location = new System.Drawing.Point(17, 162);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(160, 140);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("맑은 고딕", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button9.Location = new System.Drawing.Point(349, 308);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(160, 140);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("맑은 고딕", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button8.Location = new System.Drawing.Point(183, 308);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(160, 140);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("맑은 고딕", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button7.Location = new System.Drawing.Point(17, 308);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(160, 140);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("맑은 고딕", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button6.Location = new System.Drawing.Point(349, 163);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(160, 140);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("맑은 고딕", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(17, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 140);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("맑은 고딕", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button2.Location = new System.Drawing.Point(183, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 140);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("맑은 고딕", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button3.Location = new System.Drawing.Point(349, 17);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(160, 140);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btn_mx
+            // 
+            this.btn_mx.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_mx.Location = new System.Drawing.Point(573, 354);
+            this.btn_mx.Name = "btn_mx";
+            this.btn_mx.Size = new System.Drawing.Size(383, 56);
+            this.btn_mx.TabIndex = 11;
+            this.btn_mx.Text = "섞기";
+            this.btn_mx.UseVisualStyleBackColor = true;
+            this.btn_mx.Click += new System.EventHandler(this.btn_sf_Click);
+            // 
+            // btn_ex
+            // 
+            this.btn_ex.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_ex.Location = new System.Drawing.Point(573, 426);
+            this.btn_ex.Name = "btn_ex";
+            this.btn_ex.Size = new System.Drawing.Size(383, 55);
+            this.btn_ex.TabIndex = 12;
+            this.btn_ex.Text = "나가기";
+            this.btn_ex.UseVisualStyleBackColor = true;
+            this.btn_ex.Click += new System.EventHandler(this.btn_ex_Click);
+            // 
+            // lb_ct
+            // 
+            this.lb_ct.AutoSize = true;
+            this.lb_ct.Font = new System.Drawing.Font("맑은 고딕", 16.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lb_ct.ForeColor = System.Drawing.Color.Black;
+            this.lb_ct.Location = new System.Drawing.Point(562, 288);
+            this.lb_ct.Name = "lb_ct";
+            this.lb_ct.Size = new System.Drawing.Size(0, 38);
+            this.lb_ct.TabIndex = 13;
+            // 
+            // lb_rule
+            // 
+            this.lb_rule.BackColor = System.Drawing.Color.White;
+            this.lb_rule.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lb_rule.Font = new System.Drawing.Font("맑은 고딕", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lb_rule.Location = new System.Drawing.Point(573, 20);
+            this.lb_rule.Name = "lb_rule";
+            this.lb_rule.Size = new System.Drawing.Size(383, 245);
+            this.lb_rule.TabIndex = 14;
+            this.lb_rule.Text = "<게임 방법>\r\n1. 숫자 퍼즐을 순서대로 맞춘다.\r\n2. 메세지 박스가 뜨면 성공!\r\n3. 다시 진행하려면 섞기 버튼 클릭";
+            this.lb_rule.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(982, 503);
+            this.Controls.Add(this.lb_rule);
+            this.Controls.Add(this.lb_ct);
+            this.Controls.Add(this.btn_ex);
+            this.Controls.Add(this.btn_mx);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Puzzle Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btn_mx;
+        private System.Windows.Forms.Button btn_ex;
+        private System.Windows.Forms.Label lb_ct;
+        private System.Windows.Forms.Label lb_rule;
+    }
+}
+
---
+++ numberpuzzle/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace numberpuzzle
+{
+    public partial class Form1 : Form
+    {
+        int Counter;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void EmptyButtonChecker(Button b1, Button b2)
+        {
+            if (b2.Text == "")
+            {
+                b2.Text = b1.Text;
+                b1.Text = "";
+
+            }
+        }
+
+        private void Checker()
+        {
+            if (button1.Text == "1" && button2.Text == "2" && button3.Text == "3" &&
+               button4.Text == "4" && button5.Text == "5" && button6.Text == "6" &&
+               button8.Text == "7" && button7.Text == "8")
+            {
+                MessageBox.Show("게임에서 승리하셨습니다!", "숫자 퍼즐 게임",
+                    MessageBoxButtons.OK);
+
+
+            }
+            Counter = Counter + 1;
+            lb_ct.Text = "움직임 횟수: " + Counter;
+            
+
+        }
+
+        public void Mix()
+        {
+            int[] bnum = new int[9];
+            int i, j, row;
+            bool flag = false;
+
+            i = 1;
+            do
+            {
+                Random r = new Random();
+                row = Convert.ToInt32((r.Next(0, 8)) + 1);
+
+                for (j = 1; j <= i; j++)
+                {
+                    if (bnum[j] == row)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag == true)
+                {
+                    flag = false;
+                }
+                else
+                {
+                    bnum[i] = row;
+                    i = i + 1;
+                }
+            }
+            while (i <= 8);
+
+            button1.Text = bnum[1].ToString();
+            button2.Text = bnum[2].ToString();
+            button3.Text = bnum[3].ToString();
+            button4.Text = bnum[4].ToString();
+            button5.Text = bnum[5].ToString();
+            button6.Text = bnum[6].ToString();
+            button7.Text = "";
+            button8.Text = bnum[8].ToString();
+            button9.Text = bnum[7].ToString();
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Mix();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EmptyButtonChecker(button1, button2);
+            EmptyButtonChecker(button1, button4);
+            Checker();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            EmptyButtonChecker(button2, button1);
+            EmptyButtonChecker(button2, button3);
+            EmptyButtonChecker(button2, button5);
+            Checker();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            EmptyButtonChecker(button3, button2);
+            EmptyButtonChecker(button3, button6);
+           Checker();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            EmptyButtonChecker(button4, button1);
+            EmptyButtonChecker(button4, button5);
+            EmptyButtonChecker(button4, button7);
+            Checker();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            EmptyButtonChecker(button5, button2);
+            EmptyButtonChecker(button5, button4);
+            EmptyButtonChecker(button5, button6);
+            EmptyButtonChecker(button5, button8);
+            Checker();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            EmptyButtonChecker(button6, button3);
+            EmptyButtonChecker(button6, button5);
+            EmptyButtonChecker(button6, button9);
+            Checker();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            EmptyButtonChecker(button7, button4);
+            EmptyButtonChecker(button7, button8);
+            Checker();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            EmptyButtonChecker(button8, button5);
+            EmptyButtonChecker(button8, button7);
+            EmptyButtonChecker(button8, button9);
+            Checker();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            EmptyButtonChecker(button9, button6);
+            EmptyButtonChecker(button9, button8);
+            Checker();
+        }
+
+        private void btn_sf_Click(object sender, EventArgs e)
+        {
+            Mix();
+
+            Counter = 0;
+            lb_ct.Text = "움직임 횟수: " + Counter;
+        }
+
+        private void btn_ex_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+       
+}
